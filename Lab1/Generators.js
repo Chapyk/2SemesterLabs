4c61839c30f1dd35fcdc3515cebebe3a3edfef4c
Chapyk <chapy.kruk@gmail.com>
2026-03-01 23:40:21 +0200

    1

---
+++ Lab1/Generators.js
@@ -0,0 +1,32 @@
+function* countergen(start = 0) {
+    let current = start;
+    while (true) {
+        yield current;
+        current++
+    }
+}
+
+const gen = countergen(1)
+
+console.log(gen.next().value)
+console.log(gen.next().value)
+console.log(gen.next().value)
+
+function* fibonacciGenerator() {
+    let a = 0;
+    let b = 1;
+    while (true) {
+        yield a;
+        let next = a + b;
+        a = b;
+        b = next
+    }
+}
+
+const fibgen = fibonacciGenerator()
+
+console.log(fibgen.next().value)
+console.log(fibgen.next().value)
+console.log(fibgen.next().value)
+console.log(fibgen.next().value)
+console.log(fibgen.next().value)
